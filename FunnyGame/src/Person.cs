e07fb5b9cb170ae8b244c609f36f195cc283cd9f
Anton Vasilyev <MajorMoonMaster@gmail.com>
2017-02-13 23:20:34 +0300

    Init commit

---
+++ FunnyGame/src/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace FunnyGame
+{
+	public class Person
+	{
+		private string personName { get; };
+		private bool modeGame { get; };
+
+		public Person(string personName, bool modeGame)
+		{
+			this.personName = personName;
+			this.modeGame = modeGame;
+		}
+
+
+	}
+}
