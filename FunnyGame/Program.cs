5c3ad6ebfbedf27eff04bff79810cbb98651ff25
Anton Vasilyev <MajorMoonMaster@gmail.com>
2017-02-16 21:28:03 +0300

    Add Nlog

--- FunnyGame/Program.cs
+++ FunnyGame/Program.cs
@@ -1,12 +1,16 @@
-﻿
+﻿using NLog;
+
 namespace FunnyGame
 {
 	class Program
 	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
 		static void Main(string[] args)
 		{
+			logger.Info("The Game is start");
 			GameController game = new GameController();
 			game.Run();
+			logger.Info("The Game is end");
 		}
 	}
 }
