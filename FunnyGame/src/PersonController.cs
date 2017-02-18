b1a5940016fd5bc1d6f512094949a551921b0fee
Anton Vasilyev <MajorMoonMaster@gmail.com>
2017-02-18 13:04:24 +0300

    Refactoring

---
+++ FunnyGame/src/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FunnyGame
+{
+	public class PersonController
+	{
+		private string pattern = @"^[A-Z][a-z]+$";
+
+		public bool CheckCorrectGameName(string gameName)
+		{
+			Regex regex = new Regex(this.pattern);
+			var resultRegex = regex.Match(gameName);
+			if (resultRegex.Success)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ FunnyGameUnitTest/tests/PersonControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using FunnyGame;
+using NUnit.Framework;
+
+namespace FunnyGameUnitTest.tests
+{
+	[TestFixture]
+	public class PersonControllerTests
+	{
+		public static PersonController testPersonController = new PersonController();
+		[TestCase("Name")]
+		[TestCase("Person")]
+		public void CheckCorrectGamerNamePositivValue(string gameName)
+		{
+			bool resultCompare = testPersonController.CheckCorrectGameName(gameName);
+			Assert.IsTrue(resultCompare);
+		}
+
+		[TestCase("person")]
+		[TestCase("name")]
+		[TestCase("1Person")]
+		[TestCase("name123")]
+		[TestCase("Name1234")]
+		public void CheckCorrectGameNameNegativValue(string gameName)
+		{
+			bool resultCompare = testPersonController.CheckCorrectGameName(gameName);
+			Assert.IsFalse(resultCompare);
+		}
+	}
+}
