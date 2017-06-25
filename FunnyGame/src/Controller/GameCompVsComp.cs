e1d99d4fc8bf87f8ac21aba6f823cc212c247c32
Anton Vasilyev <MajorMoonMaster@gmail.com>
2017-06-25 14:40:09 +0300

    test

---
+++ FunnyGame/src/Controller/GameCompVsComp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FunnyGame.Controller
+{
+	public class GameCompVsComp
+	{
+		private static Random _rnd = new Random();
+
+		private static string _firstPlayerName = "Test";
+		private static string _secondPlayerName = "Test2";
+
+
+	}
+}
